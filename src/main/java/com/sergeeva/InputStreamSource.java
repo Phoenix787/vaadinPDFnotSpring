260a69280dd6685de1be45b9dbaae6893da6c965
Xenia <shiny777@bk.ru>
2017-11-09 15:10:23 +0500

    Initial commit added spring boot dependencies

---
+++ src/main/java/com/sergeeva/InputStreamSource.java
@@ -0,0 +1,20 @@
+package com.sergeeva;
+
+import com.vaadin.server.StreamResource;
+
+import java.io.InputStream;
+
+public class InputStreamSource implements StreamResource.StreamSource {
+
+    private final InputStream data;
+
+    public InputStreamSource(InputStream data) {
+        super();
+        this.data = data;
+    }
+
+    @Override
+    public InputStream getStream() {
+        return data;
+    }
+}
